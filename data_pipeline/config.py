265cc555edbd8e3e2338b7d64a9fa15d4329d15e
fernandotenorio <fnd.tenorio@gmail.com>
2026-02-08 21:19:56 -0300

    add data pipeline

---
+++ data_pipeline/config.py
@@ -0,0 +1,8 @@
+CONFIG_PIPE = {
+    "v0_data": {
+        "input_data": ["../data/raw/1.fen", "../data/raw/2.fen", "../data/raw/3.fen"],
+        "output_data": ["../data/v0/1_f.fen", "../data/v0/2_f.fen", "../data/v0/3_f.fen"],
+        "seed": [42, 7, 19]
+    },
+    
+}
