a9879a64ac858f4b75ac7423e7c2be104e98ff3b
fernandotenorio <fnd.tenorio@gmail.com>
2026-02-09 21:53:01 -0300

    fix msvc/gcc popcount, bitscan

--- Soliton/includes/Engine/defs.h
+++ Soliton/includes/Engine/defs.h
@@ -4,38 +4,15 @@
 #include <assert.h>
 #include <inttypes.h>
 
+#ifdef _MSC_VER
+#include <intrin.h>
+#pragma intrinsic(_BitScanForward64)
+#pragma intrinsic(_BitScanReverse64)
+#endif
+
 typedef uint64_t U64;
 typedef uint32_t U32;
 const U64 debruijn64 = U64(0x03f79d71b4cb0a89);
-
-#include <cstdint>
-
-#ifdef _MSC_VER
-#include <intrin.h>
-#pragma intrinsic(__popcnt64)
-#pragma intrinsic(_BitScanForward64)
-
-inline int popcountll(uint64_t value) {
-    return __popcnt64(value);
-}
-
-inline int ctzll(uint64_t value) {
-    unsigned long index;
-    if (_BitScanForward64(&index, value))
-        return static_cast<int>(index);
-    return 64;
-}
-
-#else
-inline int popcountll(uint64_t value) {
-    return __builtin_popcountll(value);
-}
-
-inline int ctzll(uint64_t value) {
-    return __builtin_ctzll(value);
-}
-#endif
-
 
 const int index64_trail[64] = {
     0,  1, 48,  2, 57, 49, 28,  3,
@@ -59,34 +36,27 @@ const int index64_lead[64] = {
    13, 18,  8, 12,  7,  6,  5, 63
 };
 
-//lsb
-inline int numberOfTrailingZeros(U64 bb){
-   assert (bb != 0);
-   //return index64_trail[((bb & -bb) * debruijn64) >> 58];
-   //return index64_trail[((bb & (~bb + 1)) * debruijn64) >> 58];
-    //return ctzll(bb);
-
-   unsigned long idx;
-   _BitScanForward64(&idx, bb);
-   return static_cast<int>(idx);
+//inline or duplicated symbol error
+inline int numberOfTrailingZeros(U64 bb) {
+    assert(bb != 0);
+#ifdef _MSC_VER
+    unsigned long index;
+    _BitScanForward64(&index, bb);
+    return index;
+#else
+    return __builtin_ctzll(bb);
+#endif
 }
 
-//msb
-inline int numberOfLeadingZeros(U64 bb){
-   assert (bb != 0);
-   /*
-   bb |= bb >> 1; 
-   bb |= bb >> 2;
-   bb |= bb >> 4;
-   bb |= bb >> 8;
-   bb |= bb >> 16;
-   bb |= bb >> 32;
-   return index64_lead[(bb * debruijn64) >> 58];
-  //return 63 - ctzll(bb);
-   */
-    unsigned long idx;
-    _BitScanReverse64(&idx, bb);
-    return static_cast<int>(idx);
+inline int numberOfLeadingZeros(U64 bb) {
+    assert(bb != 0);
+#ifdef _MSC_VER
+    unsigned long index;
+    _BitScanReverse64(&index, bb);
+    return index; // _BitScanReverse64 returns the index of MSB, which is equivalent to 63 - clz
+#else
+    return 63 - __builtin_clzll(bb);
+#endif
 }
 
 #endif
